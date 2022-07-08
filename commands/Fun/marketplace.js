0a8549684e2eb633acfbf3f85a0f7c9f0a203eb2
Michał <35614154+themsxr@users.noreply.github.com>
2022-07-08 18:34:00 +0200

    added marketplace

---
+++ commands/Fun/marketplace.js
@@ -0,0 +1,14 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: "marketplace",
+    ownerOnly: false,
+    run: async (client, message, args) => {
+
+        const arrowright = ":arrow_right:"
+
+        const embed = new MessageEmbed().setColor("#FFFFFF").setDescription(`VISIT ${arrowright} [MARKETPLACE](https://www.eyethecreator.com/discord/marketplace)`).addField("How to buy from marketplace?", "`Use: !buy [ITEM ID]`");
+    
+        message.channel.send({embeds: [embed]});
+    }
+}
